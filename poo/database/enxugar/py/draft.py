9cd8616a70bfa632e95657551c87239199e25587
oHiago <hiagobica@gmail.com>
2025-10-06 02:03:17 +0000

    enxugar

---
+++ poo/database/enxugar/py/draft.py
@@ -0,0 +1,57 @@
+class Towel:
+    def __init__(self, color: str, size: str):
+        self.color: str = color
+        self.size: str = size
+        self.wetness: int = 0
+
+    def dry(self, amount: int) -> None:
+        self.wetness += amount 
+        if self.wetness >= self.isMaxWetness():
+            self.wetness = self.isMaxWetness()
+            print ("toalha encharcada")
+
+    def isDry(self):
+        return self.wetness == 0
+    
+    def wringOut(self):
+        self.wetness = 0
+
+    def isMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20
+        if self.size == "G":
+            return 30
+        return 0
+    
+    def __str__(self) -> str:
+        return f"Cor: {self.color}, Tamanho: {self.size}, Umidade: {self.wetness}"
+    
+def main ():
+        towel: Towel =  Towel("", "")
+        while True:
+            line: str = input()
+
+            print("$" + line)
+            args: list[str] = line.split(" ")
+            
+            if args[0] == "end":
+                break
+            elif args[0] == "criar":
+                color: str = args[1]
+                size: str = args[2]
+                towel = Towel(color,size)
+            elif args[0] == "seca":
+             print("sim" if towel.isDry() else "nao")
+            elif args[0] == "torcer":
+                towel.wringOut()
+            elif args[0] == "enxugar":
+                amount: int = int(args[1])
+                towel.dry(amount)
+            elif args[0] == "mostrar":
+                print(towel)
+            else:
+                print("fail: comando não encontrado")
+
+main()
